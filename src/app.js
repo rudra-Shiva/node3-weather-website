b8a6207425143095ac58a32532478216524ccb5a
Atul Singh <atulsingh834@gmial.com>
2019-12-10 17:50:16 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const process = require('process')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
